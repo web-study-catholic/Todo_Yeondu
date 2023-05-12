e91e07097755a6604cf82713df56e8011c6c9d32
신재훈 <sjhh0105@naver.com>
2023-05-13 00:44:54 +0900

    Provider 생성

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import {Route,Routes} from 'react-router-dom'
-import Home from './pages/Home'
-import DateYear from './pages/DateYear';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import DateYear from "./pages/DateYear";
+import { TodoProvider } from "./state/TodoContext";
 
 function App() {
   return (
-    <>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/dateyear' element={<DateYear/>}/>
-    </Routes>
-    </>
+    <TodoProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dateyear" element={<DateYear />} />
+      </Routes>
+    </TodoProvider>
   );
 }
 
