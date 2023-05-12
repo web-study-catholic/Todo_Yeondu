7b19a91d73542eb9e6268846926372428d041aec
신재훈 <sjhh0105@naver.com>
2023-05-13 00:46:01 +0900

    Provider 제거

--- src/pages/DateYear.js
+++ src/pages/DateYear.js
@@ -1,16 +1,12 @@
 import React from "react";
 import Date from "../components/DateYear";
-import {TodoProvider} from '../TodoContext'
 
 const DateYear = () => {
-
   return (
     <>
-    <TodoProvider>
-      <Date/>
-    </TodoProvider>
+      <Date />
     </>
-  )
-}
+  );
+};
 
 export default DateYear;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,8 @@
-import { createGlobalStyle } from 'styled-components';
-import TodoTemplate from '../components/TodoTemplate';
-import TodoHead from '../components/TodoHead';
-import TodoList from '../components/TodoList';
-import TodoCreate from '../components/TodoCreate';
-import { TodoProvider } from '../TodoContext';
+import { createGlobalStyle } from "styled-components";
+import TodoTemplate from "../components/TodoTemplate";
+import TodoHead from "../components/TodoHead";
+import TodoList from "../components/TodoList";
+import TodoCreate from "../components/TodoCreate";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -13,14 +12,14 @@ const GlobalStyle = createGlobalStyle`
 
 function Home() {
   return (
-    <TodoProvider>
+    <>
       <GlobalStyle />
       <TodoTemplate>
         <TodoHead />
         <TodoList />
         <TodoCreate />
       </TodoTemplate>
-    </TodoProvider>
+    </>
   );
 }
 
